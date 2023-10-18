a11fb8c27417f3ed44bbdbf2e0609f4475615f32
andrhwl <120339957+andrhwl@users.noreply.github.com>
2023-10-18 23:21:38 +0300

    add

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+let total = 100;
+let names = "Bob";
+let isActive = false;
+total = "anna";
+const numbers = [1, 2, 3, true, null];
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let total: number = 100;
+let names: string = "Bob";
+let isActive: boolean = false;
+
+
+const numbers: number[] = [1, 2, 3];
